d7fc6654cb2feb088a52ee76924dae4dcfb9ccbc
25077667 <zxc25077667@gmail.com>
2018-06-24 15:57:59 +0800

    editing acceleration

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,7 @@
 //"use strict";
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
 function about_touch(){
 	let accelerometer = null;
 	try {
@@ -23,4 +26,56 @@ function about_touch(){
         	throw error;
     	}
 	}
+}
+
+var static_flag = false;
+var x_axis = 0, y_axis = 0, z_axis = 0;
+
+function onDeviceReady(){
+	navigator.accelerometer.getCurrentAcceleration(set_up, onError);
+}
+
+//誰給acceleration??
+function set_up(acceleration){
+	//陀螺離重設
+	x_axis = acceleration.x;
+	y_axis = acceleration.y;
+	z_axis = acceleration.z;
+}
+
+function onError(){
+	alert('onError!');
+}
+
+function static_flag_ctrl(){
+	static_flag = true;
+}
+
+function matching(){
+	var device = form.YourAirplaneIP.value;
+
+}
+
+function calculating(dx_axis, dy_axis, dz_axis, static_flag){
+//static_flag 是true代表穩定模式有打開
+
+	var motor_power = 0 ; //這是馬達推力的變數，100是滿格
+	if (!static_flag) {
+		dx_axis = acceleration.x - x_axis;
+		dy_axis = acceleration.y - y_axis;
+		dz_axis = acceleration.z - z_axis;
+		
+	}
+
+	else{
+		dx_axis = 0;
+		dy_axis = 0;
+		dz_axis = 0;
+		motor_power = 50;
+	}
+	return dx_axis + " " + dy_axis + " " + dz_axis + " " + motor_power + " ";
+}
+
+function send_something(){
+
 }
