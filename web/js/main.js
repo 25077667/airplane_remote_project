17480288970c655f534319e37c67498439df1f8e
25077667 <zxc25077667@gmail.com>
2018-09-17 05:24:17 +0800

    almost last funcyional function to write

--- web/js/main.js
+++ web/js/main.js
@@ -2,14 +2,12 @@
 // some resource to read https://developer.mozilla.org/zh-TW/docs/Web/API/Touch_events
 // the Generic Sensor API only support on https so tou should make the raspi enable HTTPS server
 // https://variax.wordpress.com/2017/03/18/adding-https-to-the-raspberry-pi-apache-web-server/comment-page-1/
-
-document.addEventListener("deviceready", onDeviceReady, false);
+window.addEventListener("resize", fullscreen);
 //----------declare variables---------------
 var static_flag = false;
-var x_axis = 0, y_axis = 0, z_axis = 0;
-var sensor = new Accelerometer();
+var x_axis = 0, y_axis = 0, z_axis = 9.8;
 var ip_change_record = "";
-fullscreen();
+var acceleration = new Accelerometer({frequency: 60});
 set_up();
 
 //------------ buttons-----------------------
@@ -18,21 +16,26 @@ function static_flag_ctrl(){
 	static_flag = true;
 }
 
-//誰給acceleration??
 function set_up(){
 	//加速度重設
 	//Generic Sensor API
-	var acceleration = new Accelerometer();
+	acceleration.onloading = () => model.quaternion.fromArray(acceleration.quaternion);
+	acceleration.onerror = ev => {
+		if (ev.error.name == 'NotReadableError'){console.log("Sensor is not available.");}
+	}
+	acceleration.start();
 	x_axis = acceleration.x;
 	y_axis = acceleration.y;
 	z_axis = acceleration.z;
-
+	console.log(x_axis);    //我今天不知道為啥surface 取直出來是null
+	fullscreen();
 }
 
 function fullscreen(){
 	var canvas = document.getElementById("canvas");
-	canvas.width  = jQuery(window).width()-2;
+	canvas.width  = jQuery(window).width()-2;		//canvas本身就是一種畫板，希望給牠全螢幕，方便操作
 	canvas.height = jQuery(window).height()-42;
+	console.log("screen resize");
 }
 
 //--------------touching screen------------------------
@@ -41,21 +44,21 @@ el.addEventListener("touchstart", () => { touchStart(event) }, false);
 el.addEventListener("touchend", () => { touchEnd(event) }, false);
 el.addEventListener("touchmove", () => { touchMove(event) }, false);
 
-var centorl_x = 0;
+var centorl_x = 0; //手指起點x座標
 var centorl_y = 0;
-var r = 0;
+var r = 0; //手指移動半徑
 function touchStart(event){
-	event.preventDefault();
-	var x = event.touches[0].clientX;
-	var y = event.touches[0].clientY;
-	console.log(x);
-	console.log(y);
+	event.preventDefault();  //防止滾動
+	centorl_x = event.touches[0].clientX;
+	centorl_y = event.touches[0].clientY;
+	console.log(centorl_x);
+	console.log(centorl_y);
 	var c=document.getElementById("canvas");
 	var ctx=c.getContext("2d");
 	ctx.beginPath();
-	ctx.arc(x,y,100,0,2*Math.PI);
-	ctx.stroke();
-	centorl_x = x; centorl_y =y;
+	ctx.arc(centorl_x,centorl_y,100,0,2*Math.PI);
+	ctx.stroke();	//畫圓
+	static_flag = false;  //要記的關閉""強制穩定"
 }
 
 function touchEnd(event){
@@ -71,15 +74,15 @@ function touchMove(event){
 	var x = event.touches[0].clientX;
 	var y = event.touches[0].clientY;
 	if ((x - centorl_x)**2 + (y - centorl_y)**2 > 2600 ){
-		r = 50;
+		r = 50;		//超出一定範圍禁止增長r
 	}
 	else {
 		r = Math.sqrt((x - centorl_x)**2 + (y - centorl_y)**2);
 	}
 	console.log(r)
-	document.getElementById("spot").style.setProperty('--y', y+'px');
+	document.getElementById("spot").style.setProperty('--y', y+'px');  // 更改光點(圖片)屬姓，位置屬性是變數
 	document.getElementById("spot").style.setProperty('--x', x+'px');
-	document.getElementById("spot").style.visibility = 'visible';
+	document.getElementById("spot").style.visibility = 'visible';  //光點可見
 }
 
 //----------------matchin and send_something -------------------
@@ -95,15 +98,17 @@ function matching(){
 //need to set a slice of time to get the Accelerometer value
 
 function send_something(IPAdress, msg){
-	var request = new XMLHttpRequest();
+	var request = new XMLHttpRequest();		//在JS大權上面看到的做法，聚說是很老的API
 	console.log('Trying to send:' + msg + " to " + IPAdress);
-	request.open("POST", "http://"+IPAdress);
+	request.open("POST", "https://"+IPAdress);		//https是因為Generic只能使用https
 	request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
 	request.send(msg);
 }
 
+// set a frequency function here and combine calculating() with send_something()
+
 // ------------calculating-------------
-function calculating(x_axis, y_axis, z_axis, static_flag){
+function calculating(){
 //static_flag 是true代表穩定模式有打開
 
 	var motor_power = 0 ; //這是馬達推力的變數，100是滿格
@@ -111,7 +116,7 @@ function calculating(x_axis, y_axis, z_axis, static_flag){
 		var dx_axis = acceleration.x - x_axis;
 		var dy_axis = acceleration.y - y_axis;
 		var dz_axis = acceleration.z - z_axis;
-		motor_power = touchmove(); //目前尚未宣告touchmove
+		motor_power = r;
 		//自己拿手機起來測試啦
 		//手機頭上揚是y軸增加
 		//手機左翻是x軸增加
@@ -126,16 +131,14 @@ function calculating(x_axis, y_axis, z_axis, static_flag){
 		dx_axis = 0;
 		dy_axis = 0;
 		dz_axis = 0;
-		motor_power = 50;
+		motor_power = 25;
 	}
 	return dx_axis + " " + dy_axis + " " + dz_axis + " " + motor_power + " ";
 }
 
+var cal = calculating({frequency: 60})
+//cal.start();
 //----------------error alert----------------
-
-function onDeviceReady(){
-	navigator.accelerometer.getCurrentAcceleration(set_up, onError);
-}
 
 function onError(){
 	alert('onError!');
