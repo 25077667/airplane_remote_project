a636901e6a288c114df2e7a5c32e51af6dd56966
25077667 <zxc25077667@gmail.com>
2018-10-13 00:09:22 +0800

    test

---
+++ mcu/mcu.ino/mcu.ino.ino
@@ -0,0 +1,15 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  // put your setup code here, to run once:
+  //Serial.print("hi");
+  //delay(1000);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("hello world");
+}
