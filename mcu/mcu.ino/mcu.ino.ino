0f84e1e27b0d2582b06008dcafb092080efc5527
25077667 <zxc25077667@gmail.com>
2019-02-05 20:12:51 +0800

    enhance handle_message performance

--- mcu/mcu.ino/mcu.ino.ino
+++ mcu/mcu.ino/mcu.ino.ino
@@ -3,7 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid     = "Redmi Note 5";
+const char* ssid     = "********";
 const char* password = "********";
 ESP8266WebServer server(80);
 String pattern;
@@ -12,73 +12,58 @@ void actions(int dx, int dy, int dz, int motor){
 
 }
 
+bool check_pattern(String data){
+  String copied;
+  copied = data.substring(data.length()-8, data.length()-1);
+
+  bool is_same = true;
+  int len = pattern.length();
+  while(len--)
+    if(pattern[len]!=copied[len]){
+      is_same = false;
+      break;
+    }//check every index of pattern
+  return is_same;
+}
+
 void handle_message(){
   if(server.hasArg("msg")){
-    
+
     String data = server.arg("msg");
     Serial.println(data);
-    
-    if(data.length() == 8 && pattern.length()!=0){//initial
-      //form pattern
+
+    if(pattern!="" && check_pattern(data)==false)
+      return;
+
+    if(data.length() == 8){//set pattern
       memcpy(&pattern,&data,sizeof(data));  //this is the pattern of client,first time client in
+      Serial.print("set pattern as : ");Serial.println(pattern);
     }
 
-    else if(data.length() == 11+8 ){ //disconnect
-      //form 0000000000:pattern
-      String check_pattern;
-      if(data[10]==':' && (data.substring(0,9)).toInt() == 0)
-        memcpy(&check_pattern, &(data[11]), sizeof(char)*8 );
-      else
-        return;
-
-      if(check_pattern == pattern)
-        pattern = ""; //https://www.arduino.cc/en/Tutorial/StringComparisonOperators
-      else{
-        Serial.print("Data lost or be attacked.");
-        return;
-      }
+    else if(data.length() == 11+8 ){ //disconnect, form 0000000000:pattern
+      pattern = "";
     }
 
-    else{
-      //form dx:dy:dz:motor:pattern
-      String check_pattern;
-      int pattern_index = sizeof(data)-8;
-      //will decrypt all msg
-      memcpy(&check_pattern, &(data[pattern_index]), sizeof(char)*8 );
-      /*
-        check_pattern = check_pattern XOR with pattern
+    else if(data.length() == 36){
+      /*form dx:dy:dz:motor:pattern
+        will decrypt all msg
+        check_pattern = check_pattern XOR pattern
       */
-      if(check_pattern != pattern)
-        return;
-
       String raw_dx;
       String raw_dy;
       String raw_dz;
       String raw_motor;
-      int index = 0;
-      while (data[index] != ':') {
-        raw_dx += data[index];
-        index ++;
-      }
-      index++;
-      while (data[index] != ':') {
-        raw_dy += data[index];
-        index ++;
-      }
-      index++;
-      while (data[index] != ':') {
-        raw_dz += data[index];
-        index ++;
-      }
-      index++;
-      while (data[index] != ':') {
-        raw_motor += data[index];
-        index ++;
-      }
+      memcpy(&(raw_dx[0]), &(data[0]), sizeof(char)*6);
+      memcpy(&(raw_dy[0]), &(data[7]), sizeof(char)*6);
+      memcpy(&(raw_dz[0]), &(data[14]), sizeof(char)*6);
+      memcpy(&(raw_motor[0]), &(data[21]), sizeof(char)*6);
 
       actions(raw_dx.toInt(), raw_dy.toInt(), raw_dz.toInt(), raw_motor.toInt());
     }
-
+    else{
+      Serial.print("Data lost or be attacked.");
+    }
+    server.send(200, "text/html", "Data received");
   }
 }
 
@@ -89,25 +74,23 @@ void setup() {
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.println();
-  Serial.print("wifi connecting to : ");
+  Serial.print("wifi is connecting to : ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  pinMode(D0, OUTPUT);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(700);
-    Serial.print(".");
+    digitalWrite(D0, HIGH);delay(800);digitalWrite(D0, LOW);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
   server.on("/data/",HTTP_GET,handle_message);
   server.begin();
-  Serial.print("server is on.");
-  Serial.println("");
+  Serial.println("server is on.");
 }
 
 void loop() {
