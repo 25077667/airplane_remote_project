9f19de0665b52148b032592f420252c18d9ecd8e
25077667 <zxc25077667@gmail.com>
2019-02-21 19:52:02 +0800

    set previous_data

--- mcu/mcu.ino/mcu.ino.ino
+++ mcu/mcu.ino/mcu.ino.ino
@@ -3,56 +3,72 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char* ssid     = "********";
-const char* password = "********";
+const char *ssid = "********";
+const char *password = "********";
 ESP8266WebServer server(80);
 String pattern;
 int counting = 0;
+int previous_data[4] = {0, 0, 0, 50};
 
-void actions(int dx, int dy, int dz, int motor){
+void actions(int dx, int dy, int dz, int motor)
+{
   Serial.print("Action!");
   Serial.println(counting);
   counting++;
+  //save previous action to reduce packages for transmittion
+  previous_data[0] = dx;
+  previous_data[1] = dy;
+  previous_data[2] = dz;
+  previous_data[3] = motor;
 }
 
-bool check_pattern(String data){
-  if(pattern == "")
+bool check_pattern(String data)
+{
+  if (pattern == "")
     return false;
 
   String copied;
-  copied = data.substring(data.length()-8, data.length());
+  copied = data.substring(data.length() - 8, data.length());
 
   bool is_same = true;
-  if(pattern != copied)
+  if (pattern != copied)
     is_same = false;
   return is_same;
 }
 
-void handle_message(){
-  if(server.hasArg("msg")){
+void handle_message()
+{
+  if (server.hasArg("msg"))
+  {
 
     String data = server.arg("msg");
-    
-    if(pattern!="" && check_pattern(data)==false){
+
+    if (pattern != "" && check_pattern(data) == false)
+    {
       server.send(403, "text/html", "permission denied");
       return;
     }
 
-    if(data.length() == 8){//set pattern
-      pattern = data;  //this is the pattern of client,first time client in
-      Serial.print("set pattern as : ");Serial.println(pattern);
+    if (data.length() == 8)
+    {                 //set pattern
+      pattern = data; //this is the pattern of client,first time client in
+      Serial.print("set pattern as : ");
+      Serial.println(pattern);
     }
 
-    else if(pattern == ""){
+    else if (pattern == "")
+    {
       server.send(403, "text/html", "Data lost or be attacked.");
       return;
     }
 
-    else if(data.length() == 11+8 ){ //disconnect, form 0000000000:pattern
+    else if (data.length() == 11 + 8)
+    { //disconnect, form 0000000000:pattern
       pattern = "";
     }
 
-    else if(data.length() == 36){
+    else if (data.length() == 36)
+    {
       /*form dx:dy:dz:motor:pattern
         will decrypt all msg
         check_pattern = check_pattern XOR pattern
@@ -61,19 +77,22 @@ void handle_message(){
       String raw_dy;
       String raw_dz;
       String raw_motor;
-      raw_dx = data.substring(0,6);
-      raw_dy = data.substring(7,13);
-      raw_dz = data.substring(14,20);
-      raw_motor = data.substring(21,27);
-      Serial.println(raw_dx+":"+raw_dy+":"+raw_dz+":"+raw_motor);
+      raw_dx = data.substring(0, 7);
+      raw_dy = data.substring(8, 14);
+      raw_dz = data.substring(15, 21);
+      raw_motor = data.substring(22, 28);
+      Serial.println(raw_dx + ":" + raw_dy + ":" + raw_dz + ":" + raw_motor);
       actions(raw_dx.toInt(), raw_dy.toInt(), raw_dz.toInt(), raw_motor.toInt());
     }
 
     server.send(200, "text/html", "Data received");
   }
+  else
+    actions(previous_data[0], previous_data[1], previous_data[2], previous_data[3]);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(10);
   counting = 0;
@@ -87,19 +106,23 @@ void setup() {
   WiFi.begin(ssid, password);
   pinMode(D0, OUTPUT);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(D0, HIGH);delay(800);digitalWrite(D0, LOW);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(D0, HIGH);
+    delay(800);
+    digitalWrite(D0, LOW);
   }
 
   Serial.println("\nWiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/data/",HTTP_GET,handle_message);
+  server.on("/data/", HTTP_GET, handle_message);
   server.begin();
   Serial.println("server is on.");
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
 }
