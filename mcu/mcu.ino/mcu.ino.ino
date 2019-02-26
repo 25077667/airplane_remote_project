5dba47f20b39b600f67500e46d122e639872c158
25077667 <zxc25077667@gmail.com>
2019-02-26 23:41:28 +0800

    enhance handle_message

--- mcu/mcu.ino/mcu.ino.ino
+++ mcu/mcu.ino/mcu.ino.ino
@@ -8,9 +8,9 @@ const char *password = "********";
 ESP8266WebServer server(80);
 String pattern;
 int counting = 0;
-int previous_data[4] = {0, 0, 0, 50};
+float previous_data[4] = {0, 0, 0, 50};
 
-void actions(int dx, int dy, int dz, int motor)
+void actions(float dx, float dy, float dz, float motor, bool oriented)
 {
   //if motor == 0, it is default speed; if motor > 0, it is add speed; if motor > 0, it is decrease speed;
   Serial.print("Action!");
@@ -23,73 +23,32 @@ void actions(int dx, int dy, int dz, int motor)
   previous_data[3] = motor;
 }
 
-bool check_pattern(String data)
-{
-  if (pattern == "")
-    return false;
-
-  String copied;
-  copied = data.substring(data.length() - 8, data.length());
-
-  bool is_same = true;
-  if (pattern != copied)
-    is_same = false;
-  return is_same;
-}
-
 void handle_message()
 {
-  if (server.hasArg("msg"))
+  if (server.hasArg("p"))
   {
-
-    String data = server.arg("msg");
-
-    if (pattern != "" && check_pattern(data) == false)
-    {
-      server.send(403, "text/html", "permission denied");
-      return;
-    }
-
-    if (data.length() == 8)
-    {                 //set pattern
-      pattern = data; //this is the pattern of client,first time client in
-      Serial.print("set pattern as : ");
-      Serial.println(pattern);
-    }
-
-    else if (pattern == "")
-    {
-      server.send(403, "text/html", "Data lost or be attacked.");
-      return;
-    }
-
-    else if (data.length() == 11 + 8)
-    { //disconnect, form 0000000000:pattern
+    String p = server.arg("p");
+    if (pattern != "" && (p == pattern) && server.arg("x") != "d")
+      actions(server.arg("x").toFloat(), server.arg("y").toFloat(), server.arg("z").toFloat(), server.arg("m").toFloat(), true);
+    else if (pattern != "" && (p == pattern) && server.arg("x") == "d")
       pattern = "";
-    }
+    else if (pattern == "" && (server.arg("x").toInt() + server.arg("y").toInt() + server.arg("z").toInt() + server.arg("m").toInt()) == 0)
+      pattern = p;
+    else
+      server.send(403, "text/html", "");
 
-    else if (data.length() == 40)
-    {
-      /*form dx:dy:dz:motor:pattern
-        will decrypt all msg
-        check_pattern = check_pattern XOR pattern
-      */
-      String raw_dx;
-      String raw_dy;
-      String raw_dz;
-      String raw_motor;
-      raw_dx = data.substring(0, 7);
-      raw_dy = data.substring(8, 14);
-      raw_dz = data.substring(15, 21);
-      raw_motor = data.substring(22, 28);
-      Serial.println(raw_dx + ":" + raw_dy + ":" + raw_dz + ":" + raw_motor);
-      actions(raw_dx.toInt(), raw_dy.toInt(), raw_dz.toInt(), raw_motor.toInt());
-    }
-
-    server.send(200, "text/html", "Data received");
+    server.send(200, "text/html", "");
+    return;
   }
+
+  else if (WiFi.status() != WL_CONNECTED)
+  {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+      actions(previous_data[0]/10, previous_data[1]/10, previous_data[2]/10, previous_data[3]/10, false);
+  } //it lost link, reconnection and go back
   else
-    actions(previous_data[0], previous_data[1], previous_data[2], previous_data[3]);
+    actions(previous_data[0], previous_data[1], previous_data[2], previous_data[3], true);
 }
 
 void setup()
